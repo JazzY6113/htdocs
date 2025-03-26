630392986a01e8b6fe4d3cbf0b26c272b5b911d0
JazzY6113 <kbogomolov22@gmail.com>
2025-03-26 15:34:21 +0700

    test
    # Conflicts:
    #	app/Controller/Site.php
    #	composer.json
    #	core/Src/Application.php
    #	core/Src/Route.php
    #	core/Src/Settings.php
    #	core/bootstrap.php
    #	public/index.php
    #	routes/web.php
    #	views/layouts/main.php

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -19,12 +19,3 @@ function getConfigs(string $path = DIR_CONFIG): array
 }
 
 require_once __DIR__ . '/../routes/web.php';
-$app = new Src\Application(new Src\Settings(getConfigs()));
-
-//Функция возвращает глобальный экземпляр приложения
-function app() {
-    global $app;
-    return $app;
-}
-
-return $app;
