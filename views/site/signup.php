3dbef87d6ad7c1b4c964e6ed4387906d0ad4a964
JazzY6113 <kbogomolov22@gmail.com>
2025-03-30 16:13:35 +0700

    Basic protection against CSRF attacks

--- views/site/signup.php
+++ views/site/signup.php
@@ -2,6 +2,7 @@
 <h3><?= $message ?? ''; ?></h3>
 <div class="signup">
     <form method="post">
+        <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
         <label>Имя <input type="text" name="name"></label>
         <label>Логин <input type="text" name="login"></label>
         <label>Пароль <input type="password" name="password"></label>
